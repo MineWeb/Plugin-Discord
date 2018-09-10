01f3669093fdee50aad2da0a0a0d91e519efb0c0
nivcoo <36514752+nivcoo@users.noreply.github.com>
2018-09-10 19:57:18 +0200

    Bug

--- Config/routes.php
+++ Config/routes.php
@@ -1,8 +1,3 @@
 <?php
 
 Router::connect('/discord', ['controller' => 'Discord', 'action' => 'index', 'plugin' => 'discord']);
-
-Router::connect('/admin/discord', array('controller' => 'Discord', 'action' => 'index', 'plugin' => 'Discord', 'admin' => true));
-
-
-
